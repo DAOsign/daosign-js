e4a955e25dea3cadc871086a24e9e8bb79d304d5
Brioline <mikelrap@gmail.com>
2024-03-05 17:38:23 +0200

    verifiers init

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+import * as ProofOfAuthorityTemplate from "./templates/Proof-of-Authority.json";
+import * as ProofOfSignatureTemplate from "./templates/Proof-of-Signature.json";
+import * as ProofOfAgreementTemplate from "./templates/Proof-of-Agreement.json";
+
 export interface ProofOfAuthorityVariables {
   from: string;
   agreementCID: string;
@@ -13,3 +17,23 @@ export interface ProofOfAgreementVariables {
   authorityCID: string;
   signatureCIDs: string[];
 }
+
+export type Proof = Record<string, any>;
+
+export type Template =
+  | typeof ProofOfAgreementTemplate
+  | typeof ProofOfAuthorityTemplate
+  | typeof ProofOfSignatureTemplate;
+
+export interface TypedDataField {
+  name: string;
+  type: string;
+}
+
+export enum ProofType {
+  ProofOfAuthority,
+  ProofOfSignature,
+  ProofOfAgreement,
+}
+
+export type ProofMessageTypes = Record<string, TypedDataField[]>;
